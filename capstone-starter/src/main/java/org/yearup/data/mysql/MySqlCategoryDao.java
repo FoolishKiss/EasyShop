35ab85d38e0c1f22c08d4d1cdd724828a44d367c
Brian Miller <Bmiller995@gmail.com>
2025-06-21 10:46:30 -0500

    Implemented logic gor getAllCategories()

--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,8 +5,11 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -21,7 +24,28 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     public List<Category> getAllCategories()
     {
         // get all categories
-        return ;
+        List<Category> categories = new ArrayList<>();
+ 
+        String query = """
+                SELECT *
+                FROM categories
+                """;
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet results = statement.executeQuery())
+        {
+            while (results.next())
+            {
+                categories.add(mapRow(results));
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return categories;
+
     }
 
     @Override
